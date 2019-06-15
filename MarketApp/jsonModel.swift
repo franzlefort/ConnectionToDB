156027b133caad7b46c33ec7175be1054c69fa40
Никита Кузин <franzcrypto@gmail.com>
2019-06-15 21:56:07 +0300

    Made classes for my archeticture

---
+++ MarketApp/jsonModel.swift
@@ -0,0 +1,15 @@
+//
+//  jsonModel.swift
+//  MarketApp
+//
+//  Created by Avefranz on 15/06/2019.
+//  Copyright © 2019 Avefranz. All rights reserved.
+//
+
+struct jsonModel {
+    
+    let name = String()
+    let age = Int()
+    let weight = Int()
+    let height = Int()
+}
---
+++ MarketApp/jsonParser.swift
@@ -0,0 +1,14 @@
+//
+//  jsonParser.swift
+//  MarketApp
+//
+//  Created by Avefranz on 15/06/2019.
+//  Copyright © 2019 Avefranz. All rights reserved.
+//
+
+import UIKit
+
+class jsonParser: NSObject {
+
+    
+}
